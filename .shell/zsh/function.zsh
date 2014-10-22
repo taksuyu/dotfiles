bb6392e4a26b8474a6eb3a718f7216b7a6939973
TakSuyu <taksuyu@gmail.com>
2014-10-21 17:09:18 -0700

    removed shelledit

--- .shell/zsh/function.zsh
+++ .shell/zsh/function.zsh
@@ -3,28 +3,29 @@ function cd
     builtin cd $1 && ls
 }
 
-function shelledit
-{
-    set -A ZLOGIN_SUM `sha512sum ~/.zlogin`
-    set -A ZSHENV_SUM `sha512sum ~/.zshenv`
-    set -A ZSHRC_SUM `sha512sum ~/.zshrc`
-    set -A ZPROFILE_SUM `sha512sum ~/.zprofile`
+# TODO: make this function not require an editor at all
+# function shelledit
+# {
+#     set -A ZLOGIN_SUM `sha512sum ~/.zlogin`
+#     set -A ZSHENV_SUM `sha512sum ~/.zshenv`
+#     set -A ZSHRC_SUM `sha512sum ~/.zshrc`
+#     set -A ZPROFILE_SUM `sha512sum ~/.zprofile`
 
-    $EDITOR -c "OpenSession shelledit"
+#     $EDITOR -c "OpenSession shelledit"
 
-    function compile
-    {
-        set -A FILE_SUM `sha512sum $2`
+#     function compile
+#     {
+#         set -A FILE_SUM `sha512sum $2`
 
-        # $1 is only the first argument of the array
-        if [[ $1 != $FILE_SUM[1] ]]
-        then
-            zcompile $2
-            echo "Compiling $2"
-        fi
-    }
-    compile $ZLOGIN_SUM ~/.zlogin
-    compile $ZSHENV_SUM ~/.zshenv
-    compile $ZSHRC_SUM ~/.zshrc
-    compile $ZPROFILE_SUM ~/.zprofile
-}
+#         # $1 is only the first argument of the array
+#         if [[ $1 != $FILE_SUM[1] ]]
+#         then
+#             zcompile $2
+#             echo "Compiling $2"
+#         fi
+#     }
+#     compile $ZLOGIN_SUM ~/.zlogin
+#     compile $ZSHENV_SUM ~/.zshenv
+#     compile $ZSHRC_SUM ~/.zshrc
+#     compile $ZPROFILE_SUM ~/.zprofile
+# }
