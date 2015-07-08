05af1a6f01ba3827d29bf6b851704962616fee5d
Tak Suyu <tak@ispabuse.org>
2015-07-08 03:48:45 -0700

    unused shell scripts

--- .shell/bash/python.sh
+++
@@ -1,6 +0,0 @@
-# Python virtualwrapper
-if [[ -e "/usr/bin/virtualenvwrapper.sh" ]]
-then
-    export WORKON_HOME=~/.virtualenvs
-    source /usr/bin/virtualenvwrapper.sh
-fi
--- .shell/zsh/homeshick.zsh
+++
@@ -1,5 +0,0 @@
-if [[ -d "$HOME/.homesick" ]]; then
-    # Homeshick ( https://github.com/andsens/homeshick )
-    source "$HOME/.homesick/repos/homeshick/homeshick.sh"
-    fpath=($HOME/.homesick/repos/homeshick/completions $fpath)
-fi
